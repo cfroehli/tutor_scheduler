741a7eb869269d94ec49fa0522938222cd632392
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2020-05-25 09:24:01 +0900

    More tests

--- spec/system/tickets_spec.rb
+++ spec/system/tickets_spec.rb
@@ -41,6 +41,29 @@ RSpec.describe 'Tickets', type: :system, js: true, mock_server: true do
       expect do
         post on_event_stripe_index_path, params: event, headers: headers, as: :json
       end.to change(user, :remaining_tickets).by(lot_size)
+      visit order_success_tickets_path
+      expect(page).to have_current_path(courses_path)
+      expect(page).to have_text("Your order has been processed.")
+    end
+
+    it 'can almost buy a ticket, but no thanks' do
+      product = StripeController::STRIPE_PRODUCTS.first
+      ticket_name = product[0]
+      expect do
+        click_on ticket_name
+        expect(page).to have_selector('div#stripe_params_tracker', text: 'sessionId', visible: :hidden)
+        visit order_cancel_tickets_path
+        expect(page).to have_current_path(courses_path)
+        expect(page).to have_text("Your order has been cancelled.")
+       end.to change(user, :remaining_tickets).by(0)
+    end
+
+    xit 'can start a subscription' do
+      plan = StripeController::STRIPE_PLANS.first
+      plan_name = "#{plan[0]} per month"
+      click_on plan_name
+      expect(page).to have_selector('div#stripe_params_tracker', text: 'sessionId', visible: :hidden)
+
     end
   end
 end
